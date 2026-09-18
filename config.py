2c8b8a29dec5604847ac525629134fe6890133e8
puttaswamysg1998-prog <puttaswamysg1998@gmail.com>
2026-09-18 18:21:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "ನಿಮ್ಮ_ಬೋಟ್_ಟೋಕನ್_ಇಲ್ಲಿ_ಹಾಕಿ")
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "your_api_hash")
+
+SHORTENER_URL = "vplink.in"
+SHORTENER_API = "c25d01a2545126a5e4cf855de76a253aab9be0e9"
